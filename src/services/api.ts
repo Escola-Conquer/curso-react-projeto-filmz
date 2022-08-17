7b445538c779f6bdba0b79acf165bc3cc61d5cf4
Guilherme Illescas <guilhermeillescas@Guilhermes-MacBook-Pro-2.local>
2022-08-17 20:50:32 -0300

    feat: added params

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,9 @@ const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
     api_key: import.meta.env.VITE_THE_MOVIE_DB_API_KEY,
-    language: 'pt-BR'
+    language: 'pt-BR',
+    region: 'BR',
+    include_adult: false
   }
 });
 
