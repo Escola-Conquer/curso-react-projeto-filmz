1191ec8a2cc6900baee69b9f1e3a1eca10728d65
Guilherme Illescas <guilhermeillescas@Guilhermes-MacBook-Pro-2.local>
2022-08-17 20:49:08 -0300

    fix: fixed movie types

--- src/components/MovieCard/types.ts
+++ src/components/MovieCard/types.ts
@@ -2,17 +2,8 @@ import { HTMLAttributes } from "react"
 
 import { IMovieProps } from "../../hooks/useWishlist/types"
 
-interface IMovieForCardProps {
-  id: number
-  title: string
-  overview: string
-  poster_path: string
-  vote_average: number
-  vote_count: number
-}
-
 export interface IMovieCardProps extends HTMLAttributes<HTMLDivElement> {
-  movie: IMovieForCardProps
+  movie: IMovieProps
   inWishlist: boolean
   handleAddMovieOnWishlist: (movie: IMovieProps) => void
 }
