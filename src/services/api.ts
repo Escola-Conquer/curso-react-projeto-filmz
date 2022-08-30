6d5956c99e9d5695ce2a5594ff5829a107ec53ba
Guilherme Illescas <guilhermeillescas@Guilhermes-MacBook-Pro-2.local>
2022-08-30 15:50:30 -0300

    feat: added variable in .env and fixed .env.example

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3',
+  baseURL: import.meta.env.VITE_THE_MOVIE_DB_BASE_URL,
   params: {
     api_key: import.meta.env.VITE_THE_MOVIE_DB_API_KEY,
     language: 'pt-BR',
