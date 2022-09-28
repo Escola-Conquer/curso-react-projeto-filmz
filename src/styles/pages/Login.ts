6568b7752f3afb9e553b88fbf57b5fdd2fa07a43
Mateus Santiago <mateussantiago.victor@gmail.com>
2022-09-28 10:43:16 -0300

    Adicionando página Login

---
+++ src/styles/pages/Login.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+
+export const Container = styled.main`
+  width: 100%;
+  max-width: 100vw;
+
+  h1 {
+        font-size: 4rem;
+        line-height: 4.5rem;
+
+        margin-top: 1rem;
+      }
+`
